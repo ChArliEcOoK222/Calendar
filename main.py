269d66f4203e41b1cae15cd00917c24d82866489
Charlie C <charlie2007.cook@gmail.com>
2024-09-08 11:55:03 +0100

    Access to Google Calendar

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# How do I access my Google Calendar?
 # How do I add and delete things to my calendar?
 # How do I put this into a bash script?
 # How do I run this using Alfred?
 
+### Required Libraries
+from gcsa.google_calendar import GoogleCalendar 
+
+## Listing calendar events
+gc = GoogleCalendar(credentials_path='/Users/charlie/Documents/Calendar/credentials.json')
+calendar = GoogleCalendar('charlie2007.cook@gmail.com')
+
+for event in calendar:
+    print(event)
